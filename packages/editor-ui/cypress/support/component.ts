top-rated/n8n

ci: Setup cypress component testing (no-changelog)

packages/editor-ui/cypress/support/component.ts
@@ -1,0 +1,16 @@
+import { mount } from '@cypress/vue2';
+import { createPinia, setActivePinia } from 'pinia';
+
+const initStores = () => setActivePinia(createPinia());
+
+Cypress.Commands.add('mount', mount);
+Cypress.Commands.add('initStores', initStores);
+
+declare global {
+	namespace Cypress {
+		interface Chainable {
+			mount: typeof mount;
+			initStores: typeof initStores;
+		}
+	}
+}

yksirotta/n8nnodepython

packages/editor-ui/cypress/support/component.ts
@@ -1,0 +1,16 @@
+import { mount } from '@cypress/vue';
+import { createPinia, setActivePinia } from 'pinia';
+
+const initStores = () => setActivePinia(createPinia());
+
+Cypress.Commands.add('mount', mount);
+Cypress.Commands.add('initStores', initStores);
+
+declare global {
+	namespace Cypress {
+		interface Chainable {
+			mount: typeof mount;
+			initStores: typeof initStores;
+		}
+	}
+}