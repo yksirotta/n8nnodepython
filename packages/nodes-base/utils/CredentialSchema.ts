901223c88681fa5c9b0890a325e760ef007e9de4
Elias Meire <elias@meire.dev>
2024-04-16 15:30:15 +0200

    Add toNodeProperties fn to credential schema, replace properties JSON array

--- packages/nodes-base/utils/CredentialSchema.ts
+++ packages/nodes-base/utils/CredentialSchema.ts
@@ -1,4 +1,4 @@
-import { IDisplayOptions } from 'n8n-workflow';
+import { IDisplayOptions, INodeProperties, NodePropertyTypes } from 'n8n-workflow';
 import { z } from 'zod';
 
 type GConstructor<T = {}> = new (...args: any[]) => T;
@@ -8,6 +8,7 @@ type CredentialSchemaMetadata = {
 	sensitive: boolean;
 	displayName: string;
 	displayOptions: IDisplayOptions;
+	editorRows: number;
 };
 
 function credentialSchemaMixin<
@@ -18,7 +19,13 @@ function credentialSchemaMixin<
 
 		constructor(...args: any[]) {
 			super(...args);
-			this.metadata = { hidden: false, sensitive: false, displayName: '', displayOptions: {} };
+			this.metadata = {
+				hidden: false,
+				sensitive: false,
+				displayName: '',
+				displayOptions: {},
+				editorRows: 1,
+			};
 		}
 
 		displayName(name: string) {
@@ -40,10 +47,17 @@ function credentialSchemaMixin<
 			this.metadata.displayOptions = options;
 			return this;
 		}
+
+		editorRows(rows: number) {
+			this.metadata.editorRows = rows;
+			return this;
+		}
 	};
 }
 
 const StringProperty = credentialSchemaMixin(z.ZodString);
+const NumberProperty = credentialSchemaMixin(z.ZodNumber);
+const BooleanProperty = credentialSchemaMixin(z.ZodBoolean);
 // @ts-ignore
 class EnumProperty<T> extends credentialSchemaMixin(z.ZodEnum<T>) {}
 // @ts-ignore
@@ -54,6 +68,13 @@ export const credentialSchema = {
 		return new StringProperty({
 			checks: [],
 			typeName: z.ZodFirstPartyTypeKind.ZodString,
+			coerce: false,
+		});
+	},
+	number: () => {
+		return new NumberProperty({
+			checks: [],
+			typeName: z.ZodFirstPartyTypeKind.ZodNumber,
 			coerce: false,
 		});
 	},
@@ -69,4 +90,69 @@ export const credentialSchema = {
 			typeName: z.ZodFirstPartyTypeKind.ZodNativeEnum,
 		});
 	},
+	boolean: () => {
+		return new BooleanProperty({
+			typeName: z.ZodFirstPartyTypeKind.ZodBoolean,
+			coerce: false,
+		});
+	},
+};
+
+function zodTypeToNodePropertyType(zodType: string): NodePropertyTypes {
+	switch (zodType) {
+		case 'ZodEnum':
+		case 'ZodNativeEnum':
+			return 'options';
+		case 'ZodBoolean':
+			return 'boolean';
+		case 'ZodNumber':
+			return 'number';
+		case 'ZodString':
+		default:
+			return 'string';
+	}
+}
+
+export const toNodeProperties = (schema: z.AnyZodObject): INodeProperties[] => {
+	return Object.entries(schema.shape).map(([key, prop]: [string, any]) => {
+		const metadata = prop.metadata ?? prop._def?.innerType?.metadata ?? {};
+		let property: INodeProperties = {
+			name: key,
+			type: zodTypeToNodePropertyType(prop._def.innerType?._def?.typeName ?? prop._def.typeName),
+			displayName: metadata?.displayName,
+			default: prop._def?.defaultValue?.() ?? '',
+		};
+
+		if (metadata.sensitive) {
+			if (!property.typeOptions) {
+				property.typeOptions = {};
+			}
+			property.typeOptions.password = true;
+		}
+
+		if (metadata.editorRows !== 1) {
+			if (!property.typeOptions) {
+				property.typeOptions = {};
+			}
+			property.typeOptions.rows = metadata.editorRows;
+		}
+
+		if (prop._def?.description) {
+			property.description = prop._def?.description;
+		}
+
+		if (metadata.displayOptions && Object.keys(metadata.displayOptions).length > 0) {
+			property.displayOptions = metadata.displayOptions;
+		}
+
+		const options = prop._def.innerType?._def?.values ?? prop._def.values;
+		if (options) {
+			property.options = Object.entries(options).map(([key, value]) => ({
+				name: key,
+				value: value as string,
+			}));
+		}
+
+		return property;
+	});
 };
